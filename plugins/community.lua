7ff9b2ef204af4ae4c0cb365d659ed8bd4fd5746
Dann <nicolasdpf@gmail.com>
2023-11-26 01:19:26 -0500

    Changes for my preference

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,8 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.sonokai" },
 }
